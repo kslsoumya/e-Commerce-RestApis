1b6223dffc39b270b71f20d11408f2b671fc4223
kslsoumya <37289248+kslsoumya@users.noreply.github.com>
2018-08-01 21:39:55 +0530

    Add files via upload

--- api_project.js
+++ api_project.js
@@ -3,13 +3,13 @@ define({
   "version": "0.0.1",
   "description": "Documentation for e-Commerce Application",
   "title": "e-Commerce Apis",
-  "url": "http://localhost:3000",
+  "url": "http://ecommerce-api.themeanstackpro.com",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-08-01T15:56:27.762Z",
+    "time": "2018-08-01T16:06:54.420Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
